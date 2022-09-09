ff4036826cf84576bf2540cb4a33b315aef3d757
koteitan <ukaibutton@mac.com>
2022-09-10 07:08:39 +0900

    v1.6b1.

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-document.getElementById("versiondisplay").innerHTML = "1.6b";
+document.getElementById("versiondisplay").innerHTML = "1.6b1";
 document.getElementById("changelog").innerHTML = 
 "<table align='right'><tr>"+
 "<td style='text-align:right'></td><td>2022.09.10</td><td>solver solved original problem.</td></tr>"+
