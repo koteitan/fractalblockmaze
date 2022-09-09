37dbe97e81c8b545227976844576eea2b359c22c
koteitan <ukaibutton@mac.com>
2022-09-10 06:59:24 +0900

    Revert "removed change log"
    This reverts commit e59dd16810174b5b3fb3af9506787a0c9d951be8.

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 document.getElementById("versiondisplay").innerHTML = "1.5";
 document.getElementById("changelog").innerHTML = 
 "<table align='right'><tr>"+
-"<td style='text-align:right'></td><td></td><td></td></tr>"+
+"<td style='text-align:right'></td><td>2022.09.10</td><td>solver solved original problem.</td></tr>"+
 "</table>";
 document.getElementById("knownbugs").innerHTML = 
 "<table align='right'><tr><td style='text-align:right'></td></tr></table>";
